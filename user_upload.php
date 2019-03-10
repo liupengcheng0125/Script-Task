c28e8daa74cbd00493d1c26b8a3aa1ec07f3eb63
liupengcheng0125 <liupengcheng0125@gmail.com>
2019-03-10 17:41:58 +1100

    format name and surename, validate email address and insert them into users table

--- user_upload.php
+++ user_upload.php
@@ -11,11 +11,11 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("connection failed: " . $conn->connect_error);
 } 
-echo "connection successfully";
+echo "connection successfully\n";
 
 $sql = "CREATE DATABASE IF NOT EXISTS myDB";
 if (mysqli_query($conn, $sql)) {
-    echo "create database successfully"; 
+    echo "create database successfully\n"; 
 } else {
     echo "Error creating database: " . mysqli_error($conn);
 }
@@ -30,11 +30,67 @@ email VARCHAR(50) PRIMARY KEY
 )";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Table users created successfully";
+    echo "Table users created successfully\n";
 } else {
     echo "Error creating table: " . $conn->error;
 }
  
 mysqli_close($conn);
+
+//load data from users.csv
+$row = 1;
+$stored_data = [];
+if (($handle = fopen("users.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        //echo "$num fields in line $row:\n";
+        $row++;
+        if($num == 3){
+        	array_push($stored_data, $data);
+        }
+    }
+    //var_dump($stored_data);
+    fclose($handle);
+}
+
+//insert into database
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+
+for($c = 1; $c < sizeof($stored_data); $c++){
+	// prepare and bind
+	$stmt = $conn->prepare("INSERT INTO users (name, surename, email) VALUES (?, ?, ?)");
+	$stmt->bind_param("sss", $name, $surename, $email);
+	//echo sizeof($stored_data) . "\n";
+	//echo $c;
+	// set parameters and execute
+	$name = clean(ucfirst(strtolower($stored_data[$c][0])));
+	$surename = clean(ucfirst(strtolower($stored_data[$c][1])));
+	$email = $stored_data[$c][2];
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    	echo "Error: email format is not valid!\n";
+	}
+	else{
+		$stmt->execute();
+	}
+
+}
+
+mysqli_close($conn);
+
+
+// remove special characters 
+function clean($string) {
+   $string = str_replace(' ', '', $string); // Replaces all spaces with hyphens.
+
+   return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+}
 
 ?>
